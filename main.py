ff17f15bc9d3ab91cb0fd9999119634c98521a7b
Ansh Dadwal <68729523+T-Dynamos@users.noreply.github.com>
2022-07-20 20:34:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,78 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivymd.uix.card import MDCard
+from kivymd.uix.behaviors import *
+from kivymd.uix.templates import RotateWidget
+from kivymd.uix.button import MDIconButton
+from kivy.core.window import Window
+from kivy.metrics import dp
+from kivy.clock import Clock
+from kivy.animation import Animation
+import socket 
+import os
+import select 
+import _thread
+
+class IconButton(MDIconButton,RotateWidget):
+    pass
+
+class PrivaChat(MDApp):
+
+    x = Window.size[0]
+    y = Window.size[1]
+
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.material_style = "M3"
+        return Builder.load_file("main.kv")
+
+    def on_start(self):
+        Window.size = [dp(400),dp(600)]
+
+    def animate_icon(self,instance,icon,*largs):
+        
+        anim = Animation(
+            rotate_value_angle=-360, 
+            d=0.3
+                )
+
+        anim2 = Animation(
+            opacity=0,
+            d=0.4
+                )
+
+        anim.start(instance)
+
+        def change_icon(*largs):
+
+            instance.icon = icon 
+            instance.opacity = 1
+            instance.rotate_value_angle = 1
+            anim2.start(instance)
+
+        anim2.start(instance)
+        anim2.bind(on_complete=change_icon)
+
+
+
+    def open_drawer(self,*largs):
+        animation_open = Animation(
+                pos_hint={"center_x":0.5,"center_y":0.5},
+                d=0.4
+               )
+        animation_close = Animation(
+                pos_hint={"center_x":-0.5,"center_y":0.5},
+                d=0.4
+                )
+        if self.root.ids.drawer.pos_hint == {"center_x":-0.5,"center_y":0.5}:
+            animation_open.start(self.root.ids.drawer) 
+            def set_opacity(*largs):
+                self.root.ids.drawer.md_bg_color = 0,0,0,.3
+
+            animation_open.bind(on_complete=set_opacity)
+        else:
+            self.root.ids.drawer.md_bg_color  = 0,0,0,0
+            animation_close.start(self.root.ids.drawer)
+
+
+PrivaChat().run()
